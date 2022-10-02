bb2030228999d96348b97d24a7fc79992d04d942
Rahul Sharma <koag.classroom@gmail.com>
2022-10-02 09:56:00 +0530

    added comments

--- below_threshold.py
+++ below_threshold.py
@@ -10,6 +10,7 @@ from pathlib import Path
 import time
 import os
 import sys
+import pandas as pd
 
 # get path for api file
 def resource_path(relative_path) :
@@ -23,7 +24,7 @@ def resource_path(relative_path) :
 # debug param 1 for single product
 prod_number = 12 # change to 12 for exe
 
-# store api file path
+# store-api file path
 file_path = resource_path('store.secret')
 #print(file_path)
 
@@ -41,7 +42,7 @@ headers = {
     'Content-Type': 'application/json'
 }
 
-# function to run request
+# helper function to run request
 def run_query(query, uri=store_url, headers=headers) :
     request = requests.post(
         url=uri,
@@ -54,6 +55,45 @@ def run_query(query, uri=store_url, headers=headers) :
         print(request.status_code, request)
 
 # creating query
+'''
+    data {
+        data {
+            productVariants {
+                edges {
+                    [
+                        node {
+                            displayName
+                            inventoryQuantity
+                            sku
+                            product {
+                                vendor
+                                featuredImage {
+                                    url
+                                }
+                                threshold: metafield (key,namespace) {
+                                    value
+                                }
+                                req_qty: metafield (key,namespace) {
+                                    value
+                                }
+                            }
+
+                        }
+                        .
+                        .
+                        .
+
+
+                    ]
+                }
+                pageInfo {
+                    endCursor
+                    hasNextPage
+                }
+            }
+        }
+    }
+'''
 page_info = GqlQuery().fields(['endCursor','hasNextPage'], name='pageInfo').generate()
 featured_image = GqlQuery().fields(['url'], name='featuredImage').generate()
 vendor_code = GqlQuery().fields(['value']).query(name='metafield', alias='vendor_code', input={'namespace':'"my_fields"','key':'"breadcrumb_category"'}).generate()
@@ -62,8 +102,13 @@ req_qty = GqlQuery().fields(['value']).query(name='metafield', alias='req_qty', 
 product = GqlQuery().fields(['vendor',featured_image,vendor_code,threshold,req_qty], name='product').generate()
 var_node = GqlQuery().fields(['sku','inventoryQuantity','displayName',product], name='node').generate()
 var_edges = GqlQuery().fields([var_node], name='edges').generate()
-variables = {'first':prod_number,'after':'null','query':'"(product_status:active)"'}
 
+# prepare output file
+ofile = datetime.now().strftime('%d-%m-%Y_%H-%M') + '.csv'
+with open(ofile, 'a', encoding="utf-8") as f :
+            f.write('Image,Name,SKU,Quantity,Vendor,Vendor_Code,Req_Qty\n')
+
+# extracting info from data
 '''
     Data breakdown
     data -> data -> productVariants
@@ -71,12 +116,6 @@ variables = {'first':prod_number,'after':'null','query':'"(product_status:active
     edges -> list of nodes -> node -> displayName, id, inventoryQuantity, sku, product -> vendor, featureImage, vendor_code,threshold,req_qty -> value
     pageInfo -> endCursor, startCursor, hasNextPage
 '''
-# prepare output file
-ofile = datetime.now().strftime('%d-%m-%Y_%H-%M') + '.csv'
-with open(ofile, 'a', encoding="utf-8") as f :
-            f.write('Image,Name,SKU,Quantity,Vendor,Vendor_Code,Req_Qty\n')
-
-# extracting info from data
 def info_extract(data, loop_count=1) :
     try:
         for idx,node in enumerate(data['data']['productVariants']['edges']) :
@@ -118,27 +157,33 @@ def info_extract(data, loop_count=1) :
     next_page = data['data']['productVariants']['pageInfo']['hasNextPage']
     return next_page, loop_count
 
+# flow control
 next_page = True
 first_run = True
 loop_count = 1
+# request variable
+variables = {'first':prod_number,'after':'null','query':'"(product_status:active)"'}
+
+# main loop
 while(next_page) :
     if(first_run):
         # run request for 1st time with cursor as null
         variants = GqlQuery().fields([var_edges, page_info]).query('productVariants', input=variables).operation().generate()
-    else :        
+    else : 
+        # consecutive runs with cursor = endcursor       
         variants = GqlQuery().fields([var_edges, page_info]).query('productVariants', input=variables).operation().generate()        
     #pprint(variants)    
-    data = run_query(variants)
+    data = run_query(variants) # get request data
     #pprint(data)
     #quit()
     # check if next page is present
-    next_page, loop_count = info_extract(data, loop_count)
-    # update the cursor
-    variables['after'] = '"'+data['data']['productVariants']['pageInfo']['endCursor']+'"'
-    # toggle first run
-    first_run = False
+    next_page, loop_count = info_extract(data, loop_count) # extract info from respones    
+    variables['after'] = '"'+data['data']['productVariants']['pageInfo']['endCursor']+'"' # update the cursor    
+    first_run = False # toggle first run
+
     # check for throttle condition
     time.sleep(1) # restores 50 points
+    # get current limits
     hit_rate = data['extensions']['cost']['requestedQueryCost']
     current_available = data['extensions']['cost']['throttleStatus']['currentlyAvailable']
     #print(hit_rate, current_available)
@@ -146,10 +191,11 @@ while(next_page) :
         time.sleep(10) # sleep to restore 500 points
 
 # split csv based on vendors
-import pandas as pd
 stock_data = pd.read_csv(ofile)
 
+# creating an output directory
 out_dir = './'+ofile.replace('.csv','')
 Path(out_dir).mkdir(parents=True,exist_ok=True)
+# storing vendor csv files
 for k,v in stock_data.groupby('Vendor') :
     v.to_csv(f'{out_dir}/{k}.csv')
